f8af55e769065fb993f4d21cbe6bfd54cd954b89
walsh06 <liammail06@gmail.com>
2015-03-07 12:17:20 +0000

    error found in strategy

--- PlayerStrategyShootThree.cpp
+++ PlayerStrategyShootThree.cpp
@@ -2,7 +2,7 @@
 
 PlayerStrategyShootThree::PlayerStrategyShootThree():withBallVector({0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
 {
-    int strategyMap[9][7] = {{1,9,9,9,9,9,9},
+    int strategyMap[8][7] = {{1,9,9,9,9,9,9},
                              {1,9,9,3,3,3,3},
                              {1,9,9,3,1,1,1},
                              {1,9,3,3,1,1,1},
